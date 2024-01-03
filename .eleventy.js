2d7ce804e6203c67f7608cd7ca65e42e976d2147
Lucie Kopasová <luca.kopasova@seznam.cz>
2024-01-03 18:46:33 +0100

    commit 14 automaticka galerie

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,35 @@
-module.exports = function(eleventyConfig) {
+//Nahravání galerie//
+const fastglob = require("fast-glob");
 
-	// Výchozí výstupní složka: _site
+module.exports = function (eleventyConfig) {
+  // Výchozí výstupní složka: _site
 
-	// Zkopírovat images/ do _site/images
-	eleventyConfig.addPassthroughCopy("images");
+  // Zkopírovat images/ do _site/images
+  eleventyConfig.addPassthroughCopy("images");
 
-	// Zkopírovat css/ to _site/css/
-	eleventyConfig.addPassthroughCopy("css");
+  // Zkopírovat css/ to _site/css/
+  eleventyConfig.addPassthroughCopy("css");
 
-	// Zkopírovat všechny *.js soubory na stejné umístění do _site
-	eleventyConfig.addPassthroughCopy("js");
+  // Zkopírovat všechny *.js soubory na stejné umístění do _site
+  eleventyConfig.addPassthroughCopy("js");
+
+  //Nahravání galerie//
+
+  const galerie = fastglob.sync(["images/galerie/*/*.*", "!_site"]);
+
+  eleventyConfig.addCollection("galerie", function (collection) {
+    let items = galerie.map((x) => {
+      let paths = x.split("/");
+      return {
+        gallery: paths[paths.length - 2],
+        path: x,
+        name: [paths.length - 1],
+      };
+    });
+    return items;
+  });
+
+  //konec nahravání galerie//
 
   return {
     // možné formáty šablon
@@ -19,6 +39,5 @@ module.exports = function(eleventyConfig) {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
-  }
-
+  };
 };
